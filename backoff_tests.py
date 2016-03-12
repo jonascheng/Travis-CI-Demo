199723a83d90790c24c64b3affb7bb9fdf0a1add
Jonas Cheng <jonas.cheng@gmail.com>
2016-03-12 17:02:36 +0800

    correct failure

--- backoff_tests.py
+++ backoff_tests.py
@@ -11,7 +11,7 @@ def test_full_jitter():
     for input in range(100):
         for i in range(100):
             jitter = backoff.full_jitter(input)
-        assert jitter >= input
+        assert jitter >= 0
         assert jitter <= input
 
 
